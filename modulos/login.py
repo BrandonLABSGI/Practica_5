cab9a553e9b22d4778ae4ece9ae0510bc56974bb
BrandonLABSGI <00202521@uca.edu.sv>
2025-11-01 21:20:05 -0600

    Update login.py

--- modulos/login.py
+++ modulos/login.py
@@ -35,7 +35,7 @@ def login():
         tipo = verificar_usuario(Usuario, Contra)
         if tipo:
             st.session_state["usuario"] = Usuario
-            st.session_state["tipo_usuario"] = tipo
+           
             st.success(f"Bienvenido ({Usuario}) 👋")
             st.session_state["sesion_iniciada"] = True
             st.rerun()
