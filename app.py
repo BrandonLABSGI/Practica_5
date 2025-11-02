27b00484ee2130d249192d2b867397ef3aa4d17b
BrandonLABSGI <00202521@uca.edu.sv>
2025-11-01 19:48:31 -0600

    Remove commented lines from app.py

--- app.py
+++ app.py
@@ -1,8 +1,4 @@
-# app.py
 import streamlit as st
-from modulos.venta import mostrar_venta  # Importamos la función mostrar_venta del módulo venta
-
-# Llamamos a la función mostrar_venta para mostrar el mensaje en la app
+from modulos.venta import mostrar_venta
 mostrar_venta()
-
 
